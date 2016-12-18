ac4c4c8b96f468101aeb6e9705607bc2c5844b88
Balazs Sebesteny <sebestenybalazs@gmail.com>
2016-12-18 14:41:43 +1300

    remove direct get method from the Report object - it conflicts with the ApiResource

--- Resources/Survey/Report.php
+++ Resources/Survey/Report.php
@@ -11,68 +11,62 @@ use SurveyGizmo\Helpers\SurveyGizmoException;
 class Report extends ApiResource
 {
 
-	/**
-	 * API call path 
-	 */
-	static $path = "/survey/{survey_id}/surveyreport/{id}";
+    /**
+     * API call path
+     */
+    static $path = "/survey/{survey_id}/surveyreport/{id}";
 
-	/**
-	 * Fetch list of SurveyGizmo Report Objects by survey id
-	 * @access public
-	 * @param int $survey_id - Survey ID
-	 * @param SurveyGizmo\Filter $filters - filter object
-	 * @param Array $options
-	 * @return SurveyGizmo\ApiResponse Object with SurveyGizmo\Report Objects
-	 */
-	public static function fetch($survey_id, $filters = null, $options = null) {
-		if ($survey_id < 1) {
-			throw new SurveyGizmoException(500, "Missing survey ID");
-		}
-		$response = self::_fetch(array('id' => '', 'survey_id' => $survey_id), $filter, $options);
-		return $response;
-	}
+    /**
+     * Fetch list of SurveyGizmo Report Objects by survey id
+     * @access public
+     *
+     * @param int   $survey_id - Survey ID
+     * @param array $filters
+     * @param Array $options
+     *
+     * @return SurveyGizmo\ApiResponse Object with SurveyGizmo\Report Objects
+     * @throws SurveyGizmoException
+     */
+    public static function fetch($survey_id, $filters = null, $options = null)
+    {
+        if ($survey_id < 1) {
+            throw new SurveyGizmoException(500, "Missing survey ID");
+        }
+        $response = self::_fetch(
+            [
+                'id'        => '',
+                'survey_id' => $survey_id,
+            ],
+            $filters,
+            $options
+        );
 
-	/**
-	 * Get Report Obj by survey id and report id
-	 * @access public
-	 * @param int $survey_id - survey id
-	 * @param int $id - question id
-	 * @return SurveyGizmo\Report Object
-	 */
-	public static function get($survey_id, $id)
-	{
-		if ($survey_id < 1 || $id < 1) {
-                        throw new SurveyGizmoException(500, "Missing survey ID and/or report ID");
-                }
-		return self::_get(array(
-			'survey_id' => $survey_id,
-			'id' => $id
-		));
-	}
+        return $response;
+    }
 
-	/**
-	 * Save current Report Obj
-	 * @access public
-	 * @return SurveyGizmo\ApiResponse Object with SurveyGizmo\Report Object
-	 */
-	public function save()
-	{
-		return $this->_save(array(
-			'survey_id' => $this->survey_id,
-			'id' => $this->id
-		));
-	}
+    /**
+     * Save current Report Obj
+     * @access public
+     * @return SurveyGizmo\ApiResponse Object with SurveyGizmo\Report Object
+     */
+    public function save()
+    {
+        return $this->_save([
+            'survey_id' => $this->survey_id,
+            'id'        => $this->id,
+        ]);
+    }
 
-	/**
-	 * Delete current Report Obj
-	 * @access public
-	 * @return SurveyGizmo\ApiResponse Object
-	 */
-	public function delete()
-	{
-		return self::_delete(array(
-			'survey_id' => $this->survey_id,
-			'id' => $this->id
-		));
-	}
+    /**
+     * Delete current Report Obj
+     * @access public
+     * @return SurveyGizmo\ApiResponse Object
+     */
+    public function delete()
+    {
+        return self::_delete([
+            'survey_id' => $this->survey_id,
+            'id'        => $this->id,
+        ]);
+    }
 }
