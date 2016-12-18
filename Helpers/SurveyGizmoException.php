08f74bea72549f5e6de8a8d7e8b056b17dac7e37
Balazs Sebesteny <sebestenybalazs@gmail.com>
2016-12-18 14:39:17 +1300

    make exception message optional

--- Helpers/SurveyGizmoException.php
+++ Helpers/SurveyGizmoException.php
@@ -15,7 +15,7 @@ class SurveyGizmoException extends Exception
 	protected $code = 500;
 
 	// Redefine the exception so code is first parameter
-	public function __construct($code = 0, $message, Exception $previous = null) {
+	public function __construct($code = 0, $message = '', Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 
 		// Default the message based on code (if empty)
